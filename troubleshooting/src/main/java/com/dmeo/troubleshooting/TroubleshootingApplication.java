3b1f2ecad368489c41276d12a3d6a74529f04e65
vljh246v <vljh246v@naver.com>
2023-06-28 23:23:44 +0900

    init project

---
+++ troubleshooting/src/main/java/com/dmeo/troubleshooting/TroubleshootingApplication.java
@@ -0,0 +1,13 @@
+package com.dmeo.troubleshooting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroubleshootingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroubleshootingApplication.class, args);
+	}
+
+}
---
+++ troubleshooting/src/test/java/com/dmeo/troubleshooting/TroubleshootingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmeo.troubleshooting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroubleshootingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
