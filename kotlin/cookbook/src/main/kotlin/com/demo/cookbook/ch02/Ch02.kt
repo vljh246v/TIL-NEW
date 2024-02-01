bc208237af84bff63c02396f5256444181999f83
임재현 <vljh246v@gmail.com>
2024-02-02 01:35:50 +0900

    Refactor code for null safety and readability

--- kotlin/cookbook/src/main/kotlin/com/demo/cookbook/ch02/Ch02.kt
+++ kotlin/cookbook/src/main/kotlin/com/demo/cookbook/ch02/Ch02.kt
@@ -1,9 +1,35 @@
 package com.demo.cookbook.ch02
 
 fun main() {
+
     var name: String
     name = "Test"
     // name = null
+
+
+    // 2-2
+    val jkRowling = Person("J", null, "Rowling")
+    val northWest = Person("North", null, "West")
+
+    // 2-3
+    val p1 = Person("North", null, "West")
+    if (p1.middle != null) {
+        val p1MiddleNameLength = p1.middle.length
+    }
+
+    // 2-4
+    var p2 = Person("North", null, "West")
+    if (p2.middle != null) {
+        val p2MiddleNameLength = p2.middle!!.length
+    }
+
+    // 2-5
+    var p3 = Person("North", null, "West")
+    val p3MiddleNameLength = p3.middle?.length
+
+    // 2-6
+    var p4 = Person("North", null, "West")
+    val p4MiddleNameLength = p4.middle?.length ?: 0
 }
 
 class Person(
@@ -12,5 +38,3 @@ class Person(
     val last: String
 )
 
-val jkRowling = Person("J", null, "Rowling")
-val northWest = Person("North", null, "West")
