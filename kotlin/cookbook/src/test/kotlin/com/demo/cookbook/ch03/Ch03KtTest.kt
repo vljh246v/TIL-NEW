62dc0378f3d344f629021e41515731309fc8715b
vljh246v <vljh246v@naver.com>
2024-02-03 17:02:38 +0900

    Add destructuring test case for Product class

--- kotlin/cookbook/src/test/kotlin/com/demo/cookbook/ch03/Ch03KtTest.kt
+++ kotlin/cookbook/src/test/kotlin/com/demo/cookbook/ch03/Ch03KtTest.kt
@@ -1,8 +1,10 @@
 package com.demo.cookbook.ch03
 
 import org.hamcrest.MatcherAssert
+import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers
-import org.junit.jupiter.api.Assertions
+import org.hamcrest.Matchers.closeTo
+import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
@@ -36,7 +38,7 @@ class Ch03KtTest {
         assertAll(
             { assertEquals("baseball", p2.name) },
             { MatcherAssert.assertThat(p2.price, Matchers.`is`(Matchers.closeTo(12.0, 0.01))) },
-            { Assertions.assertFalse(p2.onSale) },
+            { assertFalse(p2.onSale) },
         )
     }
 
@@ -51,6 +53,19 @@ class Ch03KtTest {
             { assertFalse(item1 === item2) },
             { assertTrue(item1.product == item2.product) },
             { assertTrue(item1.product === item2.product) }
+        )
+    }
+
+    // 3-9
+    @Test
+    fun `destructure using component functions`() {
+        val p = Product("baseball", 10.0)
+
+        val (name, price, sale) = p
+        assertAll(
+            { assertEquals(p.name, name) },
+            { assertThat(p.price, `is`(closeTo(price, 0.01))) },
+            { assertFalse(sale) },
         )
     }
 
