823f0be02d6150858ec54bba8585fe09260f1078
vljh246v <vljh246v@naver.com>
2024-02-09 20:38:52 +0900

    Add prime number functions and examples

--- kotlin/cookbook/src/main/kotlin/com/demo/cookbook/ch06/Ch06.kt
+++ kotlin/cookbook/src/main/kotlin/com/demo/cookbook/ch06/Ch06.kt
@@ -1,5 +1,8 @@
 package com.demo.cookbook.ch06
 
+import kotlin.math.ceil
+import kotlin.math.sqrt
+
 
 fun main() {
 
@@ -24,4 +27,40 @@ fun main() {
             it % 3 == 0
         }
         .first()
+
+    // 6-7
+    println(firstNPrimes(10))
+    println(primesLessThan(10))
+    println(primesLessThan2(10))
 }
+
+// 6-4
+val numSequence1 = sequenceOf(3, 1, 4, 1, 5, 9)
+val numSequence2 = listOf(3, 1, 4, 1, 5, 9).asSequence()
+
+// 6-5
+fun Int.isPrime() =
+    this == 2 || (2..ceil(sqrt(this.toDouble())).toInt())
+        .none { divisor -> this % divisor == 0 }
+
+// 6-7
+fun nextPrime(num: Int) =
+    generateSequence(num + 1) { it + 1 }
+        .first(Int::isPrime)
+
+fun firstNPrimes(count: Int) =
+    generateSequence(2, ::nextPrime)
+        .take(count)
+        .toList()
+
+// 6-8
+fun primesLessThan(max: Int): List<Int> =
+    generateSequence(2) { n -> if (n < max) nextPrime(n) else null }
+        .toList()
+        .dropLast(1)
+
+// 6-9
+fun primesLessThan2(max: Int): List<Int> =
+    generateSequence(2, ::nextPrime)
+        .takeWhile { it < max }
+        .toList()
