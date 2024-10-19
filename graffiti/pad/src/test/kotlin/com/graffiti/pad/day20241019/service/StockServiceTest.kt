d1a9de25dbcc5dd22a4711785ed20eb1ba40b17d
vljh246v <vljh246v@naver.com>
2024-10-19 13:19:16 +0900

    Add StockServiceTest with decreaseStock test method

---
+++ graffiti/pad/src/test/kotlin/com/graffiti/pad/day20241019/service/StockServiceTest.kt
@@ -0,0 +1,12 @@
+package com.graffiti.pad.day20241019.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class StockServiceTest {
+
+    @Test
+    fun decreaseStock() {
+    }
+}
