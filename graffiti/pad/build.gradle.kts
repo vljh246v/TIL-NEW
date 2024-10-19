80441170f09b4642b8ac559320d7922dc9296d35
vljh246v <vljh246v@naver.com>
2024-10-19 12:45:01 +0900

    Add MySQL connector dependency for runtime

--- graffiti/pad/build.gradle.kts
+++ graffiti/pad/build.gradle.kts
@@ -20,13 +20,17 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    runtimeOnly("mysql:mysql-connector-java")
 }
+
+
 
 kotlin {
     compilerOptions {
