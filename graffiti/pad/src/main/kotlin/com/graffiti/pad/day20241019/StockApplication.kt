ced287d1754e6ab924a5a4e812b1ce8d287e3609
vljh246v <vljh246v@naver.com>
2024-10-19 13:08:15 +0900

    Add StockApplication class for day20241019

---
+++ graffiti/pad/src/main/kotlin/com/graffiti/pad/day20241019/StockApplication.kt
@@ -0,0 +1,11 @@
+package com.graffiti.pad.day20241019
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StockApplication
+
+fun main(args: Array<String>) {
+    runApplication<StockApplication>(*args)
+}
