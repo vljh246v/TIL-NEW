c56dfa08ae2d0e57c77835c6b5b8d4cdb8d61d8e
vljh246v <vljh246v@naver.com>
2024-10-19 13:07:27 +0900

    Update MySQL connector dependency version

--- graffiti/pad/build.gradle.kts
+++ graffiti/pad/build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    runtimeOnly("mysql:mysql-connector-java")
+    runtimeOnly("com.mysql:mysql-connector-j")
 }
 
 
