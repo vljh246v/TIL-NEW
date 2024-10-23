754ce7a298f9c27050cb4f84fd62333132807e7d
vljh246v <vljh246v@naver.com>
2024-10-23 21:07:53 +0900

    Add pessimistic write lock for findByIdWithPessimisticLock

--- graffiti/pad/src/main/kotlin/com/graffiti/pad/day20241019/repository/StockRepository.kt
+++ graffiti/pad/src/main/kotlin/com/graffiti/pad/day20241019/repository/StockRepository.kt
@@ -1,8 +1,16 @@
 package com.graffiti.pad.day20241019.repository
 
+import org.hibernate.LockMode
 import com.graffiti.pad.day20241019.domain.Stock
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
+import jakarta.persistence.LockModeType
 
 interface StockRepository : JpaRepository<Stock, Long> {
     fun findByProductId(productId: Long): Stock
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT s FROM Stock s WHERE s.id = :id")
+    fun findByIdWithPessimisticLock(id: Long): Stock
 }
