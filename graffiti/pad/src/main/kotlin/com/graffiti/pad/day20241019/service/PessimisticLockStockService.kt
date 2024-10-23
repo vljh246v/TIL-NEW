69bab319f35c3678eee7b017d504e0ba9900895c
vljh246v <vljh246v@naver.com>
2024-10-23 21:10:37 +0900

    Add pessimistic locking to decreaseStock method

--- graffiti/pad/src/main/kotlin/com/graffiti/pad/day20241019/service/PessimisticLockStockService.kt
+++ graffiti/pad/src/main/kotlin/com/graffiti/pad/day20241019/service/PessimisticLockStockService.kt
@@ -1,4 +1,32 @@
 package com.graffiti.pad.day20241019.service
 
-class PessimisticLockStockService {
+import kotlin.jvm.optionals.getOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import com.graffiti.pad.day20241019.repository.StockRepository
+
+@Service
+class PessimisticLockStockService (
+    private val stockRepository: StockRepository,
+) {
+
+    @Transactional
+    fun decreaseStock(
+        id: Long,
+        quantity: Long,
+    ) {
+        val stock = stockRepository.findByIdWithPessimisticLock(id)
+        stock.quantity = stock.quantity?.let {
+                if (it - quantity < 0) {
+                    throw IllegalArgumentException("Stock is not enough")
+                } else {
+                    it - quantity
+                }
+            }
+        stockRepository.save(stock)
+    }
+
+    fun getStockQuantity(id: Long): Long {
+        return stockRepository.findById(id).getOrNull()?.quantity ?: 0
+    }
 }
