fc65eca95ec3cce3001d4e20244bd847ff3dd0dc
vljh246v <vljh246v@naver.com>
2025-01-25 09:42:25 +0900

    Update print statements to include thread information in CoroutinesTest

--- graffiti/pad/src/main/kotlin/com/graffiti/pad/day20250125/CoroutinesTest.kt
+++ graffiti/pad/src/main/kotlin/com/graffiti/pad/day20250125/CoroutinesTest.kt
@@ -11,14 +11,14 @@ class CoroutinesTest {
 fun main(): Unit = runBlocking {
     // runBlocking을 시작한 순간 코루틴 세상으로 진입
     // 새로운 코루틴 생성됨
-    println("Start")
+    printWithThread("Start")
     launch { // 반환값이 없는 코루틴을 만듬
         // runBlocking에 의한 전체 코루틴, lanuch 에 의한 newRoutine을 부르는 코루틴
 
         newRoutine()
     }
     yield() // 지금 코루틴을 중단하고 다른 코루틴이 실행되도록 스레드를 양보함
-    println("End")
+    printWithThread("End")
 }
 
 // suspend 다른 suspend 함수를 호출하는 명령어
