d71f560b7964f487abc48d6bbb4b56e2bc21dc83
vljh246v <vljh246v@naver.com>
2025-02-21 21:35:16 +0900

    Add coroutine cancellation handling in CoroutinesTest1.kt

--- graffiti/pad/src/main/kotlin/com/graffiti/pad/day20250221/CoroutinesTest1.kt
+++ graffiti/pad/src/main/kotlin/com/graffiti/pad/day20250221/CoroutinesTest1.kt
@@ -1,12 +1,20 @@
 package com.graffiti.pad.day20250221
 
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 
+/*
+코루틴이 취소에 협조하는 방법
+* kotlinx.coroutines 패키지의 suspend 함수 호출
+* isActive로 CancelationException을 던지기
+*/
 fun main(): Unit = runBlocking {
-    val job = launch {
+    val job = launch(Dispatchers.Default) {
         var i = 1
         var nextPrintTime = System.currentTimeMillis()
 
@@ -14,6 +22,10 @@ fun main(): Unit = runBlocking {
             if (nextPrintTime <= System.currentTimeMillis()) {
                 printWithThread("${i++}번째 출력")
                 nextPrintTime += 1_000L
+            }
+
+            if (isActive) {
+                throw CancellationException()
             }
         }
     }
