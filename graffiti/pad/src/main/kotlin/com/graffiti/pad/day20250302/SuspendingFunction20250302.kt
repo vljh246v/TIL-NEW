3765bab686200a26429b5bef81d57eaa793c4195
vljh246v <vljh246v@naver.com>
2025-03-02 12:16:48 +0900

    Refactor example20250302 functions and add Dispatchers

--- graffiti/pad/src/main/kotlin/com/graffiti/pad/day20250302/SuspendingFunction20250302.kt
+++ graffiti/pad/src/main/kotlin/com/graffiti/pad/day20250302/SuspendingFunction20250302.kt
@@ -1,10 +1,20 @@
 package com.graffiti.pad.day20250302
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
+fun printWithThread(str: Any) {
+    println("[${Thread.currentThread().name}] $str")
+}
+
+fun example20250302_2(): Unit = runBlocking {
+    printWithThread("Start")
+    printWithThread("Result : ${calculateResult()}")
+    printWithThread("End")
+}
 
 fun example20250302_1(): Unit = runBlocking {
     printWithThread("Start")
@@ -12,19 +22,16 @@ fun example20250302_1(): Unit = runBlocking {
     printWithThread("End")
 }
 
-fun printWithThread(str: Any) {
-    println("[${Thread.currentThread().name}] $str")
-}
-
-
-suspend fun calculateResult(): Int = coroutineScope {
-    val num1 = async {
+suspend fun calculateResult(): Int = withContext(Dispatchers.Default) {
+    val num1 = async(Dispatchers.Default) {
         delay(1_000L)
+        printWithThread("num1")
         10
     }
 
     val num2 = async {
         delay(1_000L)
+        printWithThread("num2")
         20
     }
 
