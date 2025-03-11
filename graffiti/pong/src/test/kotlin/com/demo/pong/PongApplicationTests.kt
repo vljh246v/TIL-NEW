fe1433a0bae21ba3b19bc265c75248bdf6de7564
vljh246v <vljh246v@naver.com>
2025-03-11 14:31:57 +0900

    Add initial project structure for ping and pong services

---
+++ graffiti/pong/src/test/kotlin/com/demo/pong/PongApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demo.pong
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PongApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
