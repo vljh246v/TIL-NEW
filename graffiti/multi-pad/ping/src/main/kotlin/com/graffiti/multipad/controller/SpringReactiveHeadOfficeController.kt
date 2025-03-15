5b92a980a97daf593aa246681d14dd1454c4b8cd
vljh246v <vljh246v@naver.com>
2025-03-15 15:29:03 +0900

    Add new controllers for head office and branch office

--- graffiti/multi-pad/ping/src/main/kotlin/com/graffiti/multipad/controller/SpringReactiveHeadOfficeController.kt
+++ graffiti/multi-pad/ping/src/main/kotlin/com/graffiti/multipad/controller/SpringReactiveHeadOfficeController.kt
@@ -1,5 +1,10 @@
 package com.graffiti.multipad.controller
 
+import java.time.LocalDateTime
+import java.time.LocalTime
+import org.slf4j.Logger
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -24,7 +29,7 @@ class SpringReactiveHeadOfficeController {
         .toUri()
 
     @GetMapping("/{book-id}")
-    fun getBook(@PathVariable("book-id") bookId: String): Mono<Book> {
+    fun getBook(@PathVariable("book-id") bookId: Long): Mono<Book> {
         val getBookUri = UriComponentsBuilder.fromUri(baseUri)
             .path("/{book-id}")
             .build()
@@ -37,5 +42,16 @@ class SpringReactiveHeadOfficeController {
             .uri(getBookUri)
             .retrieve()
             .bodyToMono(Book::class.java)
+    }
+
+    @Bean
+    fun run(): CommandLineRunner {
+        return CommandLineRunner { args ->
+            println("# 요청 시작 시간: ${LocalDateTime.now()}")
+
+            for(i in 0..5) {
+                var a = i
+            }
+        }
     }
 }
---
+++ graffiti/multi-pad/pong/src/main/kotlin/com/graffiti/multipad/controller/SpringReactiveBranchOfficeController.kt
@@ -0,0 +1,25 @@
+package com.graffiti.multipad.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import com.graffiti.multipad.model.Book
+import reactor.core.publisher.Mono
+
+@RequestMapping("/v1/books")
+@RestController
+class SpringReactiveBranchOfficeController {
+    val bookMap = mapOf(
+        1L to Book("1984"),
+        2L to Book("Brave New World"),
+        3L to Book("Fahrenheit 451"),
+    )
+
+    @GetMapping("/{book-id}")
+    fun getBook(@PathVariable("book-id") bookId: Long): Mono<Book> {
+        Thread.sleep(5_000L)
+        val book = bookMap[bookId]!!
+        return Mono.just(book)
+    }
+}
