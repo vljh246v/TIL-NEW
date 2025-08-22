1eb477e9db787dc6013624176722cbcecf7e1f36
vljh246v <vljh246v@naver.com>
2025-08-22 14:48:48 +0900

    fix(test.dart): change String type to dynamic for name and number variables to allow type flexibility feat(test.dart): add runtimeType prints to demonstrate type changes during execution

--- dart/practice/20250820/test.dart
+++ dart/practice/20250820/test.dart
@@ -1,7 +1,15 @@
 void main() {
-  String name = '소녀시대';
-  String name2 = '티파니';
+  dynamic name = '소녀시대';
+  print(name);
+  dynamic number = 1;
+  print(number);
 
-  print(name + name2);
-  print(name + ' ' + name2);
+  var name2 = '티파니';
+  print(name2);
+
+  print(name.runtimeType);
+  print(number.runtimeType);
+
+  name = 2;
+  print(name.runtimeType);
 }
