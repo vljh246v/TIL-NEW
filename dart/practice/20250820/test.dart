d2c19e1ea3a478015a83afb20ff17edd70157f5b
vljh246v <vljh246v@naver.com>
2025-08-22 14:43:28 +0900

    feat(test.dart): replace boolean variables with string variables to enhance functionality and add type demonstration

--- dart/practice/20250820/test.dart
+++ dart/practice/20250820/test.dart
@@ -1,7 +1,13 @@
 void main() {
-  bool isTrue = true;
-  bool isFalse = false;
+  String name = '임재현';
+  String name2 = '임재현2';
 
-  print(isTrue);
-  print(isFalse);
+  print(name);
+  print(name2);
+
+
+  var name3 = '임재현3';
+  var number = 20;
+
+  print(name3.runtimeType);
 }
