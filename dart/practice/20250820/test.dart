c5228fdcce61b0f02531c3a5aa46f1969dac248e
vljh246v <vljh246v@naver.com>
2025-08-22 14:44:47 +0900

    refactor(test.dart): update variable values and print logic to enhance output clarity and readability

--- dart/practice/20250820/test.dart
+++ dart/practice/20250820/test.dart
@@ -1,13 +1,7 @@
 void main() {
-  String name = '임재현';
-  String name2 = '임재현2';
+  String name = '소녀시대';
+  String name2 = '티파니';
 
-  print(name);
-  print(name2);
-
-
-  var name3 = '임재현3';
-  var number = 20;
-
-  print(name3.runtimeType);
+  print(name + name2);
+  print(name + ' ' + name2);
 }
