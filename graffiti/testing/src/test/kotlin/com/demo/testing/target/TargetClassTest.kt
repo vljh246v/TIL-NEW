c64e3dfdb79d18d75124ebea81f331eceb8dac54
vljh246v <vljh246v@naver.com>
2025-10-27 23:00:32 +0900

    Add Test case T1, T2

--- graffiti/testing/src/test/kotlin/com/demo/testing/target/TargetClassTest.kt
+++ graffiti/testing/src/test/kotlin/com/demo/testing/target/TargetClassTest.kt
@@ -1,8 +1,11 @@
 package com.demo.testing.target
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TargetClassTest {
     /*
     * 모든 인수가 null 일때?
@@ -12,10 +15,24 @@ class TargetClassTest {
     * str에 open은 있고 close가 없을때
     * str에 close는 있고 open이 없을때
     * str에 open, close가 여러개 있을때
-
      */
-    @Test
-    fun substringBetween() {
+
+    lateinit var targetClass: TargetClass
+
+    @BeforeAll
+    fun initAll() {
+        targetClass = TargetClass()
     }
+
+
+    @Test
+    fun strIsNullOrEmpty() {
+
+        assertThat(targetClass.substringBetween(null, "a", "b"))
+            .isEqualTo(null)
+        assertThat(targetClass.substringBetween("", "a", "b"))
+            .isEqualTo(arrayOf<String>())
+    }
+
 
 }
