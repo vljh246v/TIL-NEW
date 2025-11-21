3e8eb7cdda7ae3b54bfcb1a621597b8e751ee556
vljh246v <vljh246v@naver.com>
2025-11-21 16:47:12 +0900

    initialize Gradle project structure and add basic application and test files

---
+++ kill-batch-system/app/src/main/java/com/system/batch/config/BatchConfig.java
@@ -0,0 +1,32 @@
+package com.system.batch.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class BatchConfig extends DefaultBatchConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("org/springframework/batch/core/schema-h2.sql")
+                .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+
+}
+
+
